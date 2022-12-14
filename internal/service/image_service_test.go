359d1bb24cc1b700305903be72302482bb21f639
Dan Munro <dan@danmunro.com>
2022-12-14 07:06:20 -0800

    fix test

--- internal/service/image_service_test.go
+++ internal/service/image_service_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	"github.com/third-place/image-service/internal/test"
 	"github.com/third-place/image-service/internal/util"
 	"mime/multipart"
@@ -12,6 +13,13 @@ import (
 func createFile() (multipart.File, error) {
 	filePath := "sample.jpeg"
 	return os.Open(filePath)
+}
+
+func TestMain(m *testing.M) {
+	if os.Getenv("CI") == "" {
+		_ = godotenv.Load()
+	}
+	os.Exit(m.Run())
 }
 
 func Test_Can_UploadImage(t *testing.T) {
